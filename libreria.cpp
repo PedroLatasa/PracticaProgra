ba508e65a19666dc9b5029a5f1f3166fa8d0c40f
pabloruizburgos <114828422+pabloruizburgos@users.noreply.github.com>
2023-11-21 18:25:23 +0100

    Update libreria.cpp

--- libreria.cpp
+++ libreria.cpp
@@ -158,40 +158,146 @@ void cargaDatosCuentas(vector<Cuenta> &c)
 #define USER "pablo"
 #define PASS "aloha"
 
-bool login()
-{
-    string user, password;
+bool login(const vector<Cliente> &clientes, vector<Cuenta> &cuentas, string &dniActual) { // QQQQQQQQQQQQQQ??????????????????
+    string dni, password;
     int contador = 0;
     bool isIn = false;
 
-    do
-    {
+    do {
+        system("clear");
         cout << "\n\t\t\tLOGIN DE USUARIO\n";
         cout << "\t\t\t-----------------\n";
-        cout << "\n\tUsuario: ";
-        cin >> user;
+        cout << "\n\tDNI: ";
+        cin >> dni;
         cout << "\tPassword: ";
         cin >> password;
 
-        if (user == USER && password == PASS)
-        {
-            isIn = true;
+        for (size_t i = 0; i < clientes.size(); ++i) {
+            if (DNI_PRUEBA == dni && clientes[i].cuentas.size() > 0) {
+                // Verificar la contraseña
+                for (size_t j = 0; j < clientes[i].cuentas.size(); ++j) {
+                    if (clientes[i].cuentas[j].contraseña == password) {
+                        isIn = true;
+                        dniActual = dni;
+                        break;
+                    }
+                }
+            }
+
+            if (isIn) {
+                break;
+            }
         }
-        else
-        {
-            cout << "\tEl usuario y/o password son incorrectos.\n";
+
+        if (!isIn) {
+            cout << "\tError en el DNI o la contrasena. Intente de nuevo." << endl;
             contador++;
         }
+
     } while (contador < 3 && !isIn);
 
-    if (!isIn)
-    {
-        cout << "No ha podido acceder al sistema.\n";
-    }
-    else
-    {
-        cout << "\nBienvenido!\n";
+    if (!isIn) {
+        cout << "No ha podido acceder al sistema." << endl;
     }
 
     return isIn;
+}
+
+bool cumpleRequisitos(const string &password)
+{
+    bool tieneMayuscula = false;
+    bool tieneMinuscula = false;
+    bool tieneNumero = false;
+    bool tieneCaracterEspecial = false;
+
+    for (char c : password)
+    {
+        if (isupper(c))
+        {
+            tieneMayuscula = true;
+        }
+        else if (islower(c))
+        {
+            tieneMinuscula = true;
+        }
+        else if (isdigit(c))
+        {
+            tieneNumero = true;
+        }
+        else if (ispunct(c))
+        {
+            tieneCaracterEspecial = true;
+        }
+    }
+
+    return tieneMayuscula && tieneMinuscula && tieneNumero && tieneCaracterEspecial;
+}
+
+string pedirContrasenna(){
+    string password;
+    do
+    {
+        cout << "Ingrese la contrasena (debe contener al menos 8 caracteres con al menos una mayuscula, una minuscula, un numero y un caracter especial '?'): ";
+        cin >> password;
+        if (cumpleRequisitos(password))
+        {
+            string confirmacion;
+            cout << "Ahora confirme la contrasena: ";
+            cin >> confirmacion;
+
+            if (password == confirmacion)
+            {
+                return password;
+            }
+            else
+            {
+                cout << "Las contrasenas no coinciden. Intentelo de nuevo." << endl;
+            }
+        }
+        else
+        {
+            cout << "La contrasena no cumple con los requisitos. Intentelo de nuevo." << endl;
+        }
+    } while (true);
+}
+
+bool registro(vector<Cliente> &clientes, vector<Cuenta> &cuentas) {
+    Cliente nuevoCliente;
+    bool registrado = false;
+
+    cout << "Registro de nuevo usuario\n";
+    cout << "Ingrese el DNI del nuevo usuario: ";
+    cin >> nuevoCliente.dni;
+
+    // Verificar si el usuario ya existe
+    for (const auto &cliente : clientes) // ¿auto?
+    {
+        if (cliente.dni == nuevoCliente.dni)
+        {
+            cout << "El usuario ya existe. No se puede registrar." << endl;
+            return registrado; // Si ya existe, no se registra y se devuelve false
+        }
+    }
+
+    cout << "Ingrese el nombre: ";
+    cin >> nuevoCliente.nombre;
+    cout << "Ingrese el primer apellido: ";
+    cin >> nuevoCliente.apellido1;
+    cout << "Ingrese el segundo apellido: ";
+    cin >> nuevoCliente.apellido2;
+
+    // Solicitar y almacenar la contraseña usando la función pedirContraseña
+    string password = pedirContraseña();
+
+    // Agregar el nuevo cliente y crear una nueva cuenta
+    nuevoCliente.cuentas.push_back(Cuenta());  // Crear una cuenta vacía asociada al nuevo cliente
+    nuevoCliente.cuentas.back().Nombre_Usuario = nuevoCliente.nombre; // Usar el nombre como nombre de usuario por ahora
+    nuevoCliente.cuentas.back().Numero_Tarjeta = cuentas.size() + 1;    // Simplemente incrementar el número de tarjeta
+    nuevoCliente.cuentas.back().Fondos = 0;                             // Nuevo usuario comienza con saldo cero
+    nuevoCliente.cuentas.back().contraseña = password;                  // Almacenar la contraseña en la cuenta
+
+    clientes.push_back(nuevoCliente);  // Agregar el nuevo cliente al vector de clientes
+
+    registrado = true; // Se registra con éxito y se devuelve true
+    return registrado;
 }
