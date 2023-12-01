44e074c7d1afe96d6a27bc2bb30f11c368d7ceb3
pabloruizburgos <114828422+pabloruizburgos@users.noreply.github.com>
2023-12-01 20:27:56 +0100

    Add files via upload

---
+++ Cliente.cpp
@@ -0,0 +1,11 @@
+#include "libreria.hpp"
+
+class Cliente {
+    private:
+
+    public:
+        string dni, nombre, apellido1, apellido2, contrasenna;
+        vector<Cuenta> cuentas;
+        
+        // Cliente() {}
+};
---
+++ Cuenta.cpp
@@ -0,0 +1,32 @@
+#include "libreria.hpp"
+
+class Cuenta { // clase realizada por luis, con algunos metodos de latasa
+    private:
+
+    public:
+        string Nombre_Usuario;
+        float Fondos, Numero_Tarjeta;
+
+        void hacer_Transferencia() {
+        }
+
+        void añadir_Fondos() {
+            float cantidad;
+            cout << "Escriba la cantidad de fondos que desee añadir: " << endl;
+            cin >> cantidad;
+            Fondos += cantidad;
+        }
+
+        void retirar_Fondos() {
+            float cantidad;
+            cout << "Escriba la cantidad de fondos que desee retirar: " << endl;
+            cin >> cantidad;
+            Fondos -= cantidad;
+        }
+
+        void getName() {
+            cout << "El nombre de usuario es " + string(Nombre_Usuario) << endl;
+        }
+        
+        // Cuenta() {}
+};
