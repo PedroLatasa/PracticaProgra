c2604f56b294510f542fbafbc32bb5c0843d21e3
pabloruizburgos <114828422+pabloruizburgos@users.noreply.github.com>
2024-01-03 19:29:23 +0100

    Update libreria.hpp

--- libreria.hpp
+++ libreria.hpp
@@ -4,23 +4,23 @@
 #include <vector>
 #include <fstream>
 #include <cctype> // para las funciones de char, pero es necesario?
+//#include <cstdlib>
 using namespace std;
-
 #include "Cuenta.cpp"
 #include "Cliente.cpp"
 
 void interfaz();
-void cargaDatosClientes(vector<Cliente> &cliente);                                         // realizada por pablo
-void cargaDatosCuentas(vector<Cuenta> &c);                                                 // realizada por pablo
+void cargaDatosClientes(vector<Cliente> &cliente);                                         // realizada por pablo                                             // realizada por pablo
 int menuInicial();                                                                         // realizada por jacobo
 int menuPrincipal();                                                                       // realizada por jacobo
-int operaciones(int eleccion);
+int operaciones(int eleccion, Cliente c);
 int buscarCliente(vector<Cliente> clientes, string dni);
 int login(const vector<Cliente> clientes);    // realizada por pablo y pedro
-bool registro(vector<Cliente> &clientes, vector<Cuenta> &cuentas);                         // realizada por pedro
+bool registro(vector<Cliente> &clientes);                         // realizada por pedro
 bool cumpleRequisitos(const string &password);                                             // realizada por pedro
 string pedirContrasenna();                                                                 // realizada por pedro
 bool registro(vector<Cliente> &clientes, vector<Cuenta> &cuentas);                         // realizada por pedro
+int eligeCuenta(Cliente c);
 
 #include "libreria.cpp"
 #endif
