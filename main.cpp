7717660777ea2bfad8792f45c7e3b03381122bd5
PedroLatasa <114516964+PedroLatasa@users.noreply.github.com>
2024-01-06 13:51:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,8 +8,32 @@
 
 #include "libreria.hpp"
 
+/*void mostrarClientes(const vector<Cliente>& clientes) {
+    cout << "Lista de clientes:\n";
+    for (const auto& cliente : clientes) {
+        cout << "DNI: " << cliente.dni << "\n";
+        cout << "Nombre: " << cliente.nombre << "\n";
+        cout << "Apellido1: " << cliente.apellido1 << "\n";
+        cout << "Apellido2: " << cliente.apellido2 << "\n";
+        cout << "Contraseña: " << cliente.contrasenna << "\n";
+
+        cout << "Cuentas:\n";
+        for (const auto& cuenta : cliente.cuentas) {
+            cout << "Número de Tarjeta: " << cuenta.Numero_Tarjeta << "\n";
+            cout << "Nombre de Usuario: " << cuenta.Nombre_Usuario << "\n";
+            cout << "Fondos: " << cuenta.Fondos << "\n";
+        }
+
+        cout << "-----------------\n";
+    }
+}*/
+
 int main() // realizada por nacho basically
 {
+    /*vector<Cliente> clientes;
+    cargaDatosClientes(clientes); 
+    mostrarClientes(clientes);*/
+
     interfaz();
     return 0;
 }
