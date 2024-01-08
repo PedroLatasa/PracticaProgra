ff76b9673d61fab7a5a6f7d393a2c34883349ac4
pabloruizburgos <114828422+pabloruizburgos@users.noreply.github.com>
2024-01-08 19:43:45 +0100

    Update Activo.cpp

--- Activo.cpp
+++ Activo.cpp
@@ -1,55 +1,41 @@
 #include "libreria.hpp"
-using namespace std;
-
 
 class Activo{
-    protected:
-        float precio_actual;
-        float precio_inicial;
-
     public:
-
-        float get_rentabilidad(){
-            (precio_actual - precio_inicial) / precio_inicial;
-        }    
-
+        virtual float rentabilidad() = 0;
 };
 
 class Bono : protected Activo{
     protected:
-        float pagos;
+        float pagos, precio_inicial, precio_actual;
         
     public: 
-        float rentabilidad = get_rentabilidad_bono();
         Bono (float pa, float pi, float pg){
             pagos = pg;
             precio_actual = pa;
             precio_inicial = pi;
         }
 
-        float get_rentabilidad_bono(){
-            get_rentabilidad() + (pagos / precio_inicial);
+        float rentabilidad(){
+            return ((precio_actual-precio_inicial)/precio_inicial) + (pagos/precio_inicial);
         }
 
 };
 
 class Prestamo : protected Activo{
     protected:
-        float tipo_interes;
-        float promesa_de_pago;
+        float tipo_interes, promesa_de_pago, desembolso_inicial;
     
     public:
-        //float rentabilidad = get_rentabilidad_prestamo();
-        
-        Prestamo(float i, float pp, float pi){
+        Prestamo(float i, float pp, float di){
             tipo_interes = i;
             promesa_de_pago = pp;
-            precio_inicial = pi;
+            desembolso_inicial = di;
         }
 
-        //float get_rentabilidad_prestamo(){
-        //    (promesa_de_pago * (1 + tipo_interes)) - precio_inicial;
-        //}
+        float rentabilidad(){
+            return (promesa_de_pago * tipo_interes) - desembolso_inicial;
+        }
 
 };
 
@@ -58,7 +44,9 @@ int main(){
     Bono b34(340,300,15);
     Prestamo p82(0.1,320,310);
 
-    cout << "La rentabilidad del bono es de: " << b34.rentabilidad << endl;
+    
+    
+    cout << "La rentabilidad del bono es de: " << b34.rentabilidad() << endl;
 
     return 0;
 }
