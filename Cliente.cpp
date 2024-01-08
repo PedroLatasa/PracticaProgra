94349eee9d2b190c2edf980033ba105a0579c86f
Luis-Romero-a <120332149+Luis-Romero-a@users.noreply.github.com>
2024-01-08 09:52:21 +0100

    Add files via upload

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,11 +1,31 @@
 #include "libreria.hpp"
+#include<vector>
 
 class Cliente {
-    private:
-
-    public:
+    protected:
         string dni, nombre, apellido1, apellido2, contrasenna;
         vector<Cuenta> cuentas;
-        
+    public:
+
+        Cliente (string d, string n, string a1, string a2, string c, vector<Cuenta> ct){
+            string dni = d;
+            nombre = n;
+            apellido1 = a1;
+            apellido2 = a2;
+            contrasenna = c;
+            cuentas = ct;
+
+        }
         // Cliente() {}
+};
+
+class Cliente_Inversor : private Cliente{
+    vector<Activo> activos;
+    float rentabilidad;
+
+    public:
+        void muestra_rentabilidad(){
+            cout << "La rentabilidad de " << nombre << " " << apellido1 << " " << apellido2 << " es de: " << rentabilidad << "%. " << endl;
+        }
+
 };
