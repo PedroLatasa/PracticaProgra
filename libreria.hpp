00d920a837f76f4dbe3d10873c88b23d5fd08a43
PedroLatasa <114516964+PedroLatasa@users.noreply.github.com>
2024-01-12 14:24:18 +0100

    Update libreria.hpp

--- libreria.hpp
+++ libreria.hpp
@@ -1,6 +1,7 @@
 #ifndef _LIBRERIA
 #define _LIBRERIA
 #include <iostream>
+#include <string>
 #include <vector>
 #include <fstream>
 #include <cctype> // para las funciones de char, pero es necesario?
@@ -15,7 +16,7 @@ void interfaz();
 void cargaDatosClientes(vector<Cliente> &cliente, vector<Cuenta> &cuentas);                                         // realizada por pablo                                             // realizada por pablo
 int menuInicial();                                                                         // realizada por jacobo
 int menuPrincipal();                                                                       // realizada por jacobo
-int operaciones(int eleccion, Cliente &c);
+int operaciones(int eleccion, Cliente &c, vector<Cliente> clientes);
 int buscarCliente(vector<Cliente> clientes, string dni);
 int login(const vector<Cliente> clientes);    // realizada por pablo y pedro
 bool registro(vector<Cliente> &clientes);                         // realizada por pedro
@@ -23,10 +24,10 @@ bool esDNIValido(const string& dni);
 bool cumpleRequisitos(const string password);                                             // realizada por pedro
 string pedirContrasenna();                                                                 // realizada por pedro
 bool registro(vector<Cliente> &clientes, vector<Cuenta> &cuentas);                         // realizada por pedro
-int eligeCuenta(Cliente c);
+int eligeCuenta(Cliente &c);
 void guardaDatos(vector<Cliente> clientes, vector<Cuenta> cuentas);
 void guardaCuentas(vector<Cuenta> cuentas);
-long long generarNumeroTarjeta();
+string generarNumeroTarjeta();
 
 #include "libreria.cpp"
 #endif
