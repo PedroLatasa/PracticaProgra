f2334ed5b2ddb8164ff04a0430d1699258284fa1
PedroLatasa <114516964+PedroLatasa@users.noreply.github.com>
2024-01-14 14:26:13 +0100

    Update libreria.hpp

--- libreria.hpp
+++ libreria.hpp
@@ -21,18 +21,19 @@ int menuPrincipal();
 int menuInformacion();   
 int menuCambiarDatos();     
 void cambiarDatos(int opcion, Cliente& c);                                                            // realizada por jacobo
-void cambiarInfo(int opcion, Cliente& c);
-void operaciones(int eleccion,Cuenta& cta, Cliente& c);
+int cambiarInfo(int opcion, Cliente& c, int numCuentas);
+int darseDeBaja(Cliente &c, int numCuentas);
+void annadirFondos(vector<Cliente> &clientes, string num, float fondos);
+void realizarTransferencia(vector<Cliente> &clientes, Cuenta &cta);
+void operaciones(int eleccion,vector<Cliente> &clientes,Cuenta& cta, Cliente& c, int numCuentas);
 int buscarCliente(vector<Cliente> clientes, string dni);
 int login(const vector<Cliente> clientes);    // realizada por pablo y pedro
 bool registro(vector<Cliente> &clientes);                         // realizada por pedro
 bool esDNIValido(const string& dni);
 bool cumpleRequisitos(const string password);                                             // realizada por pedro
-string pedirContrasenna();                                                                 // realizada por pedro
-bool registro(vector<Cliente> &clientes, vector<Cuenta> &cuentas);                         // realizada por pedro
+string pedirContrasenna();                                                                 // realizada por pedro                       // realizada por pedro
 int eligeCuenta(Cliente c);
 void guardaDatos(vector<Cliente> clientes, int numCuentas);
-//void guardaCuentas(vector<Cuenta> cuentas);
 string generarNumeroTarjeta();
 
 #include "libreria.cpp"
